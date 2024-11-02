ad7d8a12090779d397069786c410bb87603db482
Vladyslav <zeross19940303@gmail.com>
2024-11-02 17:58:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,13 @@ export default defineConfig({
   plugins: [includeHtml()],
   root: 'src', // Вказуємо, що кореневою директорією є 'src'
   build: {
-    root: 'src',
     outDir: '../dist', // Вказуємо вихідну папку для збірки (поза 'src')
   },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler', // or "modern", "legacy"
+        api: 'modern', // or "modern", "legacy"
       },
     },
   },
-  base: '/cyberpunk',
 });
